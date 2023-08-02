f59cbb06329ec299d4f8958dd26379c772b7bbb9
kvammecamilla <93992120+kvammecamilla@users.noreply.github.com>
2023-08-02 08:30:56 +0200

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Test2.Services;
+using Test2.ViewModels;
+using Test2.Views;
+using Application = Microsoft.Maui.Controls.Application;
+
+namespace Test2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            DependencyService.Register<NavigationService>();
+
+            Routing.RegisterRoute(typeof(ItemDetailPage).FullName, typeof(ItemDetailPage));
+            Routing.RegisterRoute(typeof(NewItemPage).FullName, typeof(NewItemPage));
+            Routing.RegisterRoute(typeof(ChartsPage).FullName, typeof(ChartsPage));
+
+            MainPage = new MainPage();
+            // Use the NavigateToAsync<ViewModelName> method
+            // to display the corresponding view.
+            // Code lines below show how to navigate to a specific page.
+            // Comment out all but one of these lines
+            // to open the corresponding page.
+            //var navigationService = DependencyService.Get<INavigationService>();
+            //navigationService.NavigateToAsync<LoginViewModel>(true);
+
+            // MainPage = new MainPage();
+            //MainPage = new NavigationPage(new MainPage());
+        }
+    }
+}
---
+++ Item.cs
@@ -0,0 +1,17 @@
+﻿namespace Test2.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Month { get; set; }
+        public double Monthly_power { get; set; }
+        public double Monthly_cost { get; set; }
+        public double RatingLight { get; set; }
+        public double DimLight { get; set; }
+        public int NrLight { get; set; }
+        public int NrPeople { get; set; }
+        public double Distance { get; set; }
+        public double Speed { get; set; }
+        public int NrLight100 { get; internal set; }
+    }
+}
